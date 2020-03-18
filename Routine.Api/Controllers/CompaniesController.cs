95e707dc608e74993860e1e9f7e29bac0f9b0764
Tommy Zou <tommyzzj@hotmail.com>
2020-03-18 11:19:29 +0800

    Add project files.

---
+++ Routine.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Services;
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompaniesController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository??throw new ArgumentNullException(nameof(companyRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _companyRepository.GetCompaniesAsync();
+            return new JsonResult(companies);
+        }
+    }
+}
