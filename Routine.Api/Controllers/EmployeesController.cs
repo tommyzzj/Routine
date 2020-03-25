e2a70884eb3d90700c13ac028ed3834f9191e509
Tommy Zou <tzou@cimpress.com.cn>
2020-03-25 12:11:02 +0800

    Till P15

---
+++ Routine.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Models;
+using Routine.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/companies/{companyId}/employees")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICompanyRepository _companyRepository;
+
+        public EmployeesController(IMapper mapper, ICompanyRepository companyRepository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>>
+            GetEmployeesForCompany(Guid companyId)
+        {
+            if (!await _companyRepository.CompanyExistsAsync(companyId))
+            {
+                return NotFound();
+            }
+
+            var employees = await _companyRepository.GetEmployeesAsync(companyId);
+
+            var employeeDtos = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+
+            return Ok(employeeDtos);
+        }
+
+        [HttpGet("{employeeId}")]
+        public async Task<ActionResult<EmployeeDto>>
+            GetEmployeeForCompany(Guid companyId, Guid employeeId)
+        {
+            if (!await _companyRepository.CompanyExistsAsync(companyId))
+            {
+                return NotFound();
+            }
+
+            var employee = await _companyRepository.GetEmployeeAsync(companyId, employeeId);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var employeeDto = _mapper.Map<EmployeeDto>(employee);
+
+            return Ok(employeeDto);
+        }
+    }
+}
--- Routine.Api/Startup.cs
+++ Routine.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
@@ -50,6 +51,17 @@ namespace Routine.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder=> 
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Unexpected Error");
+                    });
+                });            
             }
 
             app.UseRouting();
